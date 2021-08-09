afd28803d2d3995fa338665405b63d2b6bd26d43
goswamikunal07 <53417513+goswamikunal07@users.noreply.github.com>
2021-08-10 01:22:01 +0530

    Add files via upload

---
+++ JS week 1.js
@@ -0,0 +1,6 @@
+$('.style-scope ytd-watch-flexy').attr("style","display:none")
+document.querySelector('#secondary-inner').style='display:none';
+document.querySelector("#info-contents > ytd-video-primary-info-renderer").style='display:none';
+document.querySelector("#container").style='display:none';
+document.querySelector(".style-scope ytd-video-secondary-info-renderer").style='display:none';
+
